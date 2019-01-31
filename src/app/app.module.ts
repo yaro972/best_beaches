ee031250858d27baa96517b2e8c34349f79d52d4
Thierry Aronoff <thierry.aronoff@gmail.com>
2019-01-31 21:52:39 +0100

    Create navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MainNavComponent} from './main-nav/main-nav.component';
+import {LayoutModule} from '@angular/cdk/layout';
+import {MatButtonModule, MatIconModule, MatListModule, MatSidenavModule, MatToolbarModule} from '@angular/material';
+import {RouterModule} from '@angular/router';
+import {AccueilComponent} from './accueil/accueil.component';
+import {MainRoutesModule} from './routes/main-routes/main-routes.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavComponent,
+    AccueilComponent,
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    RouterModule,
+    MainRoutesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/routes/main-routes/main-routes.module.ts
@@ -0,0 +1,18 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {AccueilComponent} from '../../accueil/accueil.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/accueil', pathMatch: 'full'},
+  {path: 'accueil', component: AccueilComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class MainRoutesModule {
+
+
+}
